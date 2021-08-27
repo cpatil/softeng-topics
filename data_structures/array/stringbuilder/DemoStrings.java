0c424a78717b93cff892897f7daf123b25b11497
joshuahenriques <joshuahenriques95@gmail.com>
2021-08-27 09:39:45 -0400

    update

---
+++ data_structures/array/stringbuilder/DemoStrings.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+public class DemoStrings {
+
+	public static void main(String[] args) {
+
+		List<String> list = Arrays.asList("oak", "pine", "fir", "ash", "birch", "elm", "yew");
+		
+		listToString(list).forEach(str -> System.out.println(str));
+	}
+
+	private static List<String> listToString(List<String> list) {
+
+		// StringBuilder
+		StringBuilder sb = new StringBuilder(32);
+
+		list.forEach(ele -> sb.append(ele).append(" "));
+
+		// String
+		String str = "";
+
+		for(String ele : list) str += ele + " ";
+
+		//StringBuffer
+		StringBuffer sbf = new StringBuffer();		
+
+		return new ArrayList<>(Arrays.asList(str, sb.toString()));;
+	}
+}
