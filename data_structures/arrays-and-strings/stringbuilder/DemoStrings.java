e716a9e0029f980cedf3b4e20a40cd55e2538d33
joshuahenriques <joshuahenriques95@gmail.com>
2021-09-05 14:47:04 -0400

    add stacks and queues notes

--- data_structures/arrays-and-strings/stringbuilder/DemoStrings.java
+++ data_structures/arrays-and-strings/stringbuilder/DemoStrings.java
@@ -23,9 +23,42 @@ public class DemoStrings {
 
 		for(String ele : list) str += ele + " ";
 
-		//StringBuffer
-		StringBuffer sbf = new StringBuffer();		
-
 		return new ArrayList<>(Arrays.asList(str, sb.toString()));
 	}
+
+		public void palindrome() {
+			String palindrome = "Dot saw I was Tod";
+			int len = palindrome.length();
+			char[] tempCharArray = new char[len];
+			char[] charArray = new char[len];
+			
+			// put original string in an 
+			// array of chars
+			for (int i = 0; i < len; i++) {
+				tempCharArray[i] = 
+					palindrome.charAt(i);
+			} 
+			
+			// reverse array of chars
+			for (int j = 0; j < len; j++) {
+				charArray[j] =
+					tempCharArray[len - 1 - j];
+			}
+			
+			String reversePalindrome =
+				new String(charArray);
+			System.out.println(reversePalindrome);
+		}
+
+			public void builderPalindrome() {
+				String palindrome = "Dot saw I was Tod";
+				 
+				StringBuilder sb = new StringBuilder(palindrome);
+				
+				sb.reverse();  // reverse it
+				
+				System.out.println(sb);
+			}
+		
+	
 }
